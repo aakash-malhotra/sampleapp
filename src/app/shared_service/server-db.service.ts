77b77e88b4d32f342bb6dd2fae64098c7ae288fd
ThammaiahNJ <kishanthammaiah.kodava@gmail.com>
2020-05-28 23:23:19 +0530

    saved

--- src/app/shared_service/server-db.service.ts
+++ src/app/shared_service/server-db.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/Rx';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerDbService {
+  private baseUrl:string='http:/localhost:8080/api';
+  private headers = new Headers({'Content-Type':'application/json'});
+  private options = new RequestOptions({headers:this.headers});
+  constructor(private _http:Http) { }
 
-  constructor() { }
+  getServers(){
+    return this._http.get(this.baseUrl+'/servers', this.options).pipe(Map(Response:Response)=>Response.json());
+  }
 }
