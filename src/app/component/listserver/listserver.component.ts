b44fd7a12fd60557e87030dd2d6ab48a086eefbc
ThammaiahNJ <kishanthammaiah.kodava@gmail.com>
2020-05-29 16:54:30 +0530

    thirdcommitt

--- src/app/component/listserver/listserver.component.ts
+++ src/app/component/listserver/listserver.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ServerDbService} from '../../shared_service/server-db.service';
+import {ServerDb} from '../../server-db';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-listserver',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListserverComponent implements OnInit {
 
-  constructor() { }
+  public servers:any;
+  constructor(private _serverDbService:ServerDbService, private _router:Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+   this._serverDbService.getServers().subscribe((servers)=>{
+     console.log(servers);
+     this.servers=servers;
+   },(error)=>{
+     console.log(error);
+   });
   }
-
+  deleteserver(server){
+    this._serverDbService.deleteServer(server.sno).subscribe((data)=>{
+      this.servers.splice(this.servers.indexOf(server), 1);
+    },(error)=>{
+      console.log(error);
+    });
+  }
+  updateserver(server){
+    this._serverDbService.setter(server);
+    this._router.navigate(['/op']);
+  }
+  newserver(){
+   let server=new ServerDb();
+    this._serverDbService.setter(server);
+    this._router.navigate(['/op']);
+  }
 }
+
+
+
--- src/app/component/server-db-form/server-db-form.component.ts
+++ src/app/component/server-db-form/server-db-form.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ServerDb} from '../../server-db';
+import {Router} from '@angular/router';
+import {ServerDbService} from '../../shared_service/server-db.service';
+import { create } from 'domain';
+import { createServer } from 'https';
+import { error } from 'protractor';
+
 
 @Component({
   selector: 'app-server-db-form',
@@ -6,10 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-db-form.component.css']
 })
 export class ServerDbFormComponent implements OnInit {
-
-  constructor() { }
+private server:ServerDb
+  constructor(private _serverDbService:ServerDbService, private _router: Router) { }
 
   ngOnInit(): void {
+    this.server=this._serverDbService.getter()
   }
 
+  processForm(){
+    if(this.server.sno==undefined){
+      this._serverDbService.createServer(this.server).subscribe((server)=>{
+        console.log(server);
+        this._router.navigate(['/']);
+      },(error)=>{
+        console.log(error);
+        this._router.navigate(['/']);
+
+      });
+    }else
+    {
+        this._serverDbService.updateServer(this.server).subscribe((server)=>{
+          console.log(server);
+        },(error)=>{
+          console.log(error);
+        });
+      }
+    
+  }
 }
